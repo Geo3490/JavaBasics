1211e7daba5480f0fc452182fd8598bcef4b8485
Geo3490 <geo40127@gmail.com>
2022-01-03 20:42:16 -0500

    String concat

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,11 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String firstName = "George";
+    String lastName = "Elefsiades";
+    System.out.println(firstName + "," + lastName);
+
+    }
+}
