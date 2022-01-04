da9f77dc51eef7d661418c74869d47a4967b82ac
Geo3490 <geo40127@gmail.com>
2022-01-03 20:48:19 -0500

    updated

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	String firstName = "George";
     String lastName = "Elefsiades";
-    System.out.println(firstName + "," + lastName);
+    System.out.println(firstName + " " + lastName);
 
     }
 }
