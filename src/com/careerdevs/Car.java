7e31d258384179c1310ecd430e0d440d5788644e
Geo3490 <geo40127@gmail.com>
2022-01-11 18:36:50 -0500

    Car-Project

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,51 @@
+package com.careerdevs;
+
+public class Car {
+
+    //FIELDS
+    public String make;
+    public String model;
+    public int mileage;
+    public float gas;
+
+    //CONSTRUCTOR
+    public Car(String make, String model, int mileage, float gas) {
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.gas = gas;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+        public void setName (String make){
+            this.make = make;
+
+        }
+        public String getModel () {
+            return model;
+        }
+
+
+        public void setModel (String model){
+            this.model = model;
+        }
+
+        public int getMileage () {
+            return mileage;
+        }
+
+        public void getMileage ( int mileage){
+            this.mileage = 1000;
+        }
+
+        public float getGas () {
+            return gas;
+        }
+    public void setGas(float gas) {
+        this.gas = 10;
+
+    }
+}
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,29 +1,17 @@
 package com.careerdevs;
 
-//import java.util.Arrays;
 
-//public class Main {
-//    public static void main (String[] args) {
-//        String fullName = new String("Hello");
-//        System.out.println(fullName.length());
-//        System.out.println(fullName.toLowerCase());
-//        System.out.println(fullName.concat(" " + "My Name Is George"));
-//        System.out.println(fullName + " "+ "My Name Is George");
-//        byte [][] fullNameNew = {{34},{44}};
-//
-////        System.out.println();
-//    }
-//}
+public class Main{
 
-import java.util.Scanner;
+    public static void main(String[] args) {
 
-public class Main {
-    public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Name:");
-        String name = scanner.nextLine();
-        System.out.print("Age:");
-        Byte age = scanner.nextByte();
-        System.out.println("Welcome"+ " " + name + " " + "Your Age is" + " " + age);
+        Car myCar = new Car("Camarro2SS", "Chevy", 10000, 10);
+
+        System.out.println(myCar.getMake());
+        System.out.println(myCar.getModel());
+        System.out.println(myCar.getMileage());
+        System.out.println(myCar.getGas());
+
+
     }
 }
